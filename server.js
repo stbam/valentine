bc6c18b121c0ddd4c59565a4c8d7bcbcf8cc2b41
stbam <55661516+stbam@users.noreply.github.com>
2026-02-13 21:29:08 -0500

    new commit

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files
 app.use(express.static(__dirname));
+
+// Serve index.html on root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
